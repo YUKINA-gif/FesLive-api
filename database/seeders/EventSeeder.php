635b2d457311aa7f267e57ab7ec3cbfce826795c
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-07-06 14:08:12 +0900

    seeder追加

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -27,5 +27,70 @@ class EventSeeder extends Seeder
         DB::table("events")->insert(
             $param
         );
+
+        $param = [
+            "name" => "RUSH BALL",
+            "tw_account" => "RUSH_BALL",
+            "image" => "https://pbs.twimg.com/profile_images/1344910758719541248/MfcaWJ1N_normal.jpg",
+            "address" => "大阪府泉大津市夕凪町泉大津フェニックス多目的広場",
+            "event_start_date" => "2021-08-28",
+            "event_last_date" => "2021-08-29",
+        ];
+        DB::table("events")->insert(
+            $param
+        );
+
+        $param = [
+            "name" => "ROCK IN JAPAN FESTIVAL",
+            "tw_account" => "rockinon_fes",
+            "image" => "http://pbs.twimg.com/profile_images/1402096348485218324/R28q_FKN_normal.jpg",
+            "address" => "茨城県ひたちなか市馬渡字大沼605-4",
+            "event_start_date" => "2021-08-07",
+            "event_2_date" => "2021-08-08",
+            "event_3_date" => "2021-08-09",
+            "event_4_date" => "2021-08-14",
+            "event_last_date" => "2021-08-15",
+        ];
+        DB::table("events")->insert(
+            $param
+        );
+
+        $param = [
+            "name" => "FM802 RADIO CRAZY",
+            "tw_account" => "FM802RADIOCRAZY",
+            "image" => "https://pbs.twimg.com/profile_images/1324683656594481152/UsDXRqDY_normal.jpg",
+            "address" => "大阪府大阪市住之江区南港北1丁目5-102",
+            "event_start_date" => "2021-12-26",
+            "event_last_date" => "2021-12-27",
+        ];
+        DB::table("events")->insert(
+            $param
+        );
+
+        $param = [
+            "name" => "MONSTER baSH",
+            "tw_account" => "MONSTERbaSH_",
+            "image" => "https://pbs.twimg.com/profile_images/1278980516989960192/fgkYgyoe_normal.jpg",
+            "address" => "香川県仲多度郡まんのう町吉野4243-12",
+            "event_start_date" => "2021-08-21",
+            "event_2_date" => "2021-08-22",
+            "event_last_date" => "2021-08-28",
+        ];
+        DB::table("events")->insert(
+            $param
+        );
+
+        $param = [
+            "name" => "WILD BUNCH FEST.",
+            "tw_account" => "WildBunchFest",
+            "image" => "https://pbs.twimg.com/profile_images/1048200166480609280/30sqYalX_normal.jpg",
+            "address" => "山口県山口市阿知須山口きらら博記念公園",
+            "event_start_date" => "2020-09-19",
+            "event_2_date" => "2020-09-20",
+            "event_last_date" => "2020-09-21",
+        ];
+        DB::table("events")->insert(
+            $param
+        );
     }
 }
